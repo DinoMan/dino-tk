8c29c0ab19d0bf195986a3d75360bcc60189ac92
Konstantinos Vougioukas <kvougiou@cg2212.doc.ic.ac.uk>
2019-07-03 11:59:16 +0100

    Added functionality to search for matching filenames for multimodal data

--- dtk/filesystem/__init__.py
+++ dtk/filesystem/__init__.py
@@ -11,3 +11,36 @@ def list_files(folder, file_filter=None):
                     file_list.append(file_path)
 
     return file_list
+
+
+def list_matching_files(directories, ext=None, file_filter=None):
+    if file_filter is None:
+        file_filter = Filter(is_file=True)
+
+    matching_files = []
+    matching_dirs = []
+    for root, dirs, files in os.walk(directories[0], topdown=False):
+        for name in files:
+            file_name = os.path.splitext(name)[0]
+            extension = os.path.splitext(name)[1]
+
+            if ext is not None and extension != ext[0]:
+                continue
+
+            sub_path = root.replace(directories[0], "")
+            if sub_path != '' and sub_path[0] == '/':
+                sub_path = sub_path[1:]
+
+            criteria_pass = True
+            for i in range(1, len(directories)):
+                if not file_filter(os.path.join(directories[i], sub_path, file_name + ext[i])):
+                    criteria_pass = False
+                    break
+
+            if not criteria_pass:
+                continue
+
+            matching_dirs.append(sub_path)
+            matching_files.append(file_name)
+
+    return {"files": matching_files, "dirs": matching_dirs}
