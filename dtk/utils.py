2ac94c26a9d69c6a46171ef23f3afcb57514c5cd
Dino Vougioukas <1238366+DinoMan@users.noreply.github.com>
2020-07-03 17:02:20 +0100

    Added 2D point tracker

--- dtk/utils.py
+++ dtk/utils.py
@@ -1,5 +1,6 @@
 from functools import reduce
 import operator
+import numpy as np
 
 
 def args2dict(args):
@@ -56,3 +57,44 @@ def group_factors(factors, optimal_factor=4):
 
     groups.append(reduce(operator.mul, group, 1))
     return sorted(groups)
+
+
+class Point2DTrackerKF():
+    def __init__(self, starting_points, fps=25, process_noise=100, detector_accuracy=25):
+        dt = 1 / fps
+        self.F = np.array([[1, dt], [0, 1]])
+        self.Q = process_noise * np.array([[(dt ** 3) / 3, (dt ** 2) / 2], [(dt ** 2) / 2, dt]])
+        self.R = detector_accuracy
+
+        self.x = np.c_[starting_points.flatten(), np.zeros_like(starting_points.flatten())]
+        self.P = np.zeros((2 * len(starting_points), 2, 2))
+        self.P[:, 0, 0] = detector_accuracy
+        self.P[:, 1, 1] = detector_accuracy
+        self.H = np.expand_dims(np.array([1, 0]), 0)
+
+    def reset(self, starting_points):
+        self.x = np.c_[starting_points.flatten(), np.zeros_like(starting_points.flatten())]
+        self.P = np.zeros((2 * len(starting_points), 2, 2))
+
+    def get_current_estimate(self):
+        no_points = self.P.shape[0]
+        return np.reshape(self.x[:, 0], (no_points // 2, 2)).copy()
+
+    def predict(self):
+        no_points = self.P.shape[0]
+        for i in range(no_points):
+            self.x[i] = np.matmul(self.F, self.x[i].T)
+            self.P[i] = np.matmul(np.matmul(self.F, self.P[i]), self.F.T) + self.Q
+
+        return np.reshape(self.x[:, 0], (no_points // 2, 2)).copy()
+
+    def update(self, measurement):
+        z = measurement.flatten()
+        no_points = self.P.shape[0]
+        for i in range(no_points):
+            innovation = z[i] - np.matmul(self.H, self.x[i])
+            innov_cov = np.matmul(np.matmul(self.H, self.P[i]), self.H.T) + self.R
+            kalman_gain = np.matmul(self.P[i], self.H.T) / innov_cov  # Innovation in this case is scalar so we can just elementwise divide
+            self.x[i] = self.x[i] + np.matmul(kalman_gain, innovation)
+            self.P[i] = np.matmul(np.eye(2) - np.matmul(kalman_gain, self.H), self.P[i])
+        return np.reshape(self.x[:, 0], (no_points // 2, 2)).copy()
