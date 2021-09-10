3478d592de51b4971395032b5fbe3a4c4b683260
Dino Vougioukas <1238366+DinoMan@users.noreply.github.com>
2021-09-10 16:08:46 +0100

    prepared for v0.2 release

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup
 
 setup(name='dtk',
-      version='0.1',
+      version='0.2',
       description='Dino\'s library for machine learning and other things',
       author='Dino Vougioukas',
       author_email='dinovgk@gmail.com',
       url='https://github.com/DinoMan/dino-tk.git',
-      download_url='https://github.com/DinoMan/dino-tk/v0.1.tar.gz',
+      download_url='https://github.com/DinoMan/dino-tk/archive/refs/tags/v0.2.tar.gz',
       keywords=['machine-learning', 'toolkit', 'file operations'],
       license='MIT',
       packages=['dtk', 'dtk.filesystem', 'dtk.metrics', 'dtk.nn', 'dtk.nn.temporal', 'dtk.transforms', 'dtk.speech', 'dtk.media'],
