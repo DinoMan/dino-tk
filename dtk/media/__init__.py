fixed issues with spectrogram in 16bit precision, cleaned up code

dtk/media/__init__.py
@@ -36,7 +36,7 @@ def save_audio(path, audio, audio_rate=16000):
 
 
 def format_spectrogram(spectrogram, contrast=1, colormap=cv2.COLORMAP_BONE, normalize=False):
-    img = spectrogram.squeeze().detach().cpu().numpy()
+    img = spectrogram.squeeze().detach().cpu().numpy().astype(np.float32)
     if normalize:
         img = np.clip(img - img.mean(), -img.std(), img.std())
 

dtk/nn/temporal/attention.py
@@ -131,7 +131,7 @@ class RelPositionMultiHeadedAttention(MultiHeadedAttention):
         :param torch.Tensor x: (batch, time, size)
         :param bool zero_triu: return the lower triangular part of the matrix
         """
-        zero_pad = torch.zeros((*x.size()[:3], 1), device=x.device, dtype=x.dtype)
+        zero_pad = torch.zeros((*x.size()[:3], 1)).type_as(x)
         x_padded = torch.cat([zero_pad, x], dim=-1)
 
         x_padded = x_padded.view(*x.size()[:2], x.size(3) + 1, x.size(2))

dtk/nn/temporal/embedding.py
@@ -57,8 +57,7 @@ class PositionalEncoding(torch.nn.Module):
         """Reset the positional encodings."""
         if self.pe is not None:
             if self.pe.size(1) >= x.size(1):
-                if self.pe.dtype != x.dtype or self.pe.device != x.device:
-                    self.pe = self.pe.to(dtype=x.dtype, device=x.device)
+                self.pe = self.pe.type_as(x)
                 return
         pe = torch.zeros(x.size(1), self.d_model)
         if self.reverse:
@@ -74,7 +73,7 @@ class PositionalEncoding(torch.nn.Module):
         pe[:, 0::2] = torch.sin(position * div_term)
         pe[:, 1::2] = torch.cos(position * div_term)
         pe = pe.unsqueeze(0)
-        self.pe = pe.to(device=x.device, dtype=x.dtype)
+        self.pe = pe.type_as(x)
 
     def forward(self, x: torch.Tensor):
         """Add positional encoding.

dtk/transforms/__init__.py
@@ -83,7 +83,7 @@ class RandomCrop(object):
 def normalize_vector(v):
     batch = v.shape[0]
     v_mag = torch.sqrt(v.pow(2).sum(1))
-    v_mag = torch.max(v_mag, torch.autograd.Variable(torch.FloatTensor([1e-8]).cuda()))
+    v_mag = torch.max(v_mag, torch.autograd.Variable(torch.FloatTensor([1e-8]).to(v.device)))
     v_mag = v_mag.view(batch, 1).expand(batch, v.shape[1])
     v = v / v_mag
     return v

dtk/transforms/video.py
@@ -93,8 +93,8 @@ def normalize(clip, mean, std, inplace=False):
     assert _is_tensor_video_clip(clip), "clip should be a 4D torch.tensor"
     if not inplace:
         clip = clip.clone()
-    mean = torch.as_tensor(mean, dtype=clip.dtype, device=clip.device)
-    std = torch.as_tensor(std, dtype=clip.dtype, device=clip.device)
+    mean = torch.as_tensor(mean).type_as(clip)
+    std = torch.as_tensor(std).type_as(clip)
     clip.sub_(mean[None, :, None, None]).div_(std[None, :, None, None])
     return clip
 